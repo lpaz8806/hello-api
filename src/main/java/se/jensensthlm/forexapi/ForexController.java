4255ff153c99faeb51e7e57c395ed02e5ad5b547
Luis Paz <lpaz8806@gmail.com>
2022-08-15 14:30:45 +0200

    Update ForexController.java

--- src/main/java/se/jensensthlm/forexapi/ForexController.java
+++ src/main/java/se/jensensthlm/forexapi/ForexController.java
@@ -3,13 +3,18 @@ package se.jensensthlm.forexapi;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/exchange-rate")
+@RequestMapping("api/v1/exchange-rate/")
 public class ForexController {
-
     private ForexExchangeProvider exchangeProvider = new ForexExchangeProvider();
 
-    @GetMapping("{sourceCurrency}/{targetCurrency}")
-    public double getExchangeRate(@PathVariable String sourceCurrency, @PathVariable String targetCurrency) {
-        return 0.0;
-    }
+    // implement here the exchange endpoint
+    // feel free to use the convention you want
+    // e.g.
+    
+    // api/v1/exchange-rate/usd/sek
+
+    // api/v1/exchange-rate
+    /*
+        { source: "SEK", target: "USD" }
+    */
 }
